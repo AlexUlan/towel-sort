e64b154377ef1dd98cff2ecec9858df5f97c5806
AlexUlan <alexander33.xata@gmail.com>
2020-10-07 10:47:17 +0200

    init: task towel-sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix = []) {
+    let gluedArray = [];
+    matrix.forEach((element, index) => {
+        let temp = index % 2 !== 0 ? element.reverse() : element;
+        gluedArray = [...gluedArray, ...temp];
+    });
+    return gluedArray;
+};
